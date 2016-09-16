004a1b3fea2ddb811069b4807dae2a1a20f5c51f
ASM <cpp.create@gmail.com>
2016-09-16 14:15:37 +0300

    Now usable in C++ with tests and unique names

--- bson.h
+++ bson.h
@@ -1,88 +1,113 @@
 #ifndef __BSON_MACRO_H__
 #define __BSON_MACRO_H__
-#define EMPTY()
-#define DEFER(id) id EMPTY()
-#define OBSTRUCT(...) __VA_ARGS__ DEFER(EMPTY)()
-#define EXPAND(...) __VA_ARGS__
-#define EVAL(...)  EVAL1(EVAL1(EVAL1(__VA_ARGS__)))
-#define EVAL1(...) EVAL2(EVAL2(EVAL2(__VA_ARGS__)))
-#define EVAL2(...) EVAL3(EVAL3(EVAL3(__VA_ARGS__)))
-#define EVAL3(...) EVAL4(EVAL4(EVAL4(__VA_ARGS__)))
-#define EVAL4(...) EVAL5(EVAL5(EVAL5(__VA_ARGS__)))
-#define EVAL5(...) __VA_ARGS__
-/* #define EVAL5(...) EVAL6(EVAL6(EVAL6(__VA_ARGS__)))
-#define EVAL6(...) EVAL7(EVAL7(EVAL7(__VA_ARGS__)))
-#define EVAL7(...) __VA_ARGS__*/
-#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
-#define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
+#ifdef __cplusplus
+extern "C" {
+#endif
+#define BS_EMPTY()
+#define BS_DEFER(id) id BS_EMPTY()
+#define BS_OBSTRUCT(...) __VA_ARGS__ BS_DEFER(BS_EMPTY)()
+#define BS_EXPAND(...) __VA_ARGS__
+#define BS_EVAL(...)  BS_EVAL1(BS_EVAL1(BS_EVAL1(__VA_ARGS__)))
+#define BS_EVAL1(...) BS_EVAL2(BS_EVAL2(BS_EVAL2(__VA_ARGS__)))
+#define BS_EVAL2(...) BS_EVAL3(BS_EVAL3(BS_EVAL3(__VA_ARGS__)))
+#define BS_EVAL3(...) BS_EVAL4(BS_EVAL4(BS_EVAL4(__VA_ARGS__)))
+#define BS_EVAL4(...) BS_EVAL5(BS_EVAL5(BS_EVAL5(__VA_ARGS__)))
+#define BS_EVAL5(...) __VA_ARGS__
+#define BS_CAT(a, ...) BS_PRIMITIVE_CAT(a, __VA_ARGS__)
+#define BS_PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
 
-#define INC(x) PRIMITIVE_CAT(INC_, x) 
-#define INC_0 1
+#define BS_INC(x) BS_PRIMITIVE_CAT(BS_INC_, x) 
+#define BS_INC_0 1
 #include "inc.h"
 
-#define DEC(x) PRIMITIVE_CAT(DEC_, x)
-#define DEC_0 0
+#define BS_DEC(x) BS_PRIMITIVE_CAT(BS_DEC_, x)
+#define BS_DEC_0 0
 #include "dec.h"
 
-#define CHECK_N(x, n, ...) n
-#define CHECK(...) CHECK_N(__VA_ARGS__, 0,)
+#define BS_CHECK_N(x, n, ...) n
+#define BS_CHECK(...) BS_CHECK_N(__VA_ARGS__, 0,)
 
-#define NOT(x) CHECK(PRIMITIVE_CAT(NOT_, x))
-#define NOT_0 ~, 1,
+#define BS_NOT(x) BS_CHECK(BS_PRIMITIVE_CAT(BS_NOT_, x))
+#define BS_NOT_0 ~, 1,
 
-#define COMPL(b) PRIMITIVE_CAT(COMPL_, b)
-#define COMPL_0 1
-#define COMPL_1 0
+#define BS_COMPL(b) BS_PRIMITIVE_CAT(BS_COMPL_, b)
+#define BS_COMPL_0 1
+#define BS_COMPL_1 0
 
-#define BOOL(x) COMPL(NOT(x))
+#define BS_BOOL(x) BS_COMPL(BS_NOT(x))
 
-#define IIF(c) PRIMITIVE_CAT(IIF_, c)
-#define IIF_0(t, ...) __VA_ARGS__
-#define IIF_1(t, ...) t
+#define BS_IIF(c) BS_PRIMITIVE_CAT(BS_IIF_, c)
+#define BS_IIF_0(t, ...) __VA_ARGS__
+#define BS_IIF_1(t, ...) t
 
-#define IF(c) IIF(BOOL(c))
+#define BS_IF(c) BS_IIF(BS_BOOL(c))
 
-#define EAT(...)
-#define EXPAND(...) __VA_ARGS__
-#define WHEN(c) IF(c)(EXPAND, EAT)
+#define BS_EAT(...)
+#define BS_EXPAND(...) __VA_ARGS__
+#define BS_WHEN(c) BS_IF(c)(BS_EXPAND, BS_EAT)
 
-#define REPEAT(count, macro, ...) WHEN(count)(\
-    OBSTRUCT(REPEAT_INDIRECT) ()(DEC(count), macro, __VA_ARGS__) \
-    OBSTRUCT(macro)(DEC(count), __VA_ARGS__) \
+#define BS_REPEAT(count, macro, ...) BS_WHEN(count)(\
+    BS_OBSTRUCT(BS_REPEAT_INDIRECT) ()(BS_DEC(count), macro, __VA_ARGS__) \
+    BS_OBSTRUCT(macro)(BS_DEC(count), __VA_ARGS__) \
 )
-#define REPEAT_INDIRECT() REPEAT
+#define BS_REPEAT_INDIRECT() BS_REPEAT
 
-#define TO_ARRAY_EL(i, v) v[i],
-#define TO_ARRAY(v, len) REPEAT(len, TO_ARRAY_EL, v)
+#define BS_TO_ARRAY_EL(i, v) v[i],
+#define BS_TO_ARRAY(v, len) BS_REPEAT(len, BS_TO_ARRAY_EL, v)
 
-#define TO_ARRAY_REV_EL(i, v, len) v[len-i],
-#define TO_ARRAY_REV(v, len) REPEAT(len, TO_ARRAY_EL, v, len)
+#define BS_TO_ARRAY_REV_EL(i, v, len) v[len-i],
+#define BS_TO_ARRAY_REV(v, len) BS_REPEAT(len, BS_TO_ARRAY_EL, v, len)
 
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define TO_ENDIANED_ARRAY TO_ARRAY
+#define BS_TO_ENDIANED_ARRAY BS_TO_ARRAY
 #else
-#define TO_ENDIANED_ARRAY TO_ARRAY_REV
+#define BS_TO_ENDIANED_ARRAY BS_TO_ARRAY_REV
 #endif
-#define FIRST1(...) FIRST(__VA_ARGS__)
-#define FIRST(e, ...) e
-#define NOTFIRST1(...) NOTFIRST(__VA_ARGS__)
-#define NOTFIRST(e, ...) __VA_ARGS__
-#define DOCUMENT(len, ...) EVAL(OBSTRUCT(DOCUMENT_IT)(len, 5, __VA_ARGS__) '\x0')
-#define DOCUMENT_IT(len, size, ...) IF(len)(DOCUMENT_BODY, DOCUMENT_HEAD)(len, size, __VA_ARGS__)
-#define DOCUMENT_BODY(len, size, t, k, klen, v, ...) OBSTRUCT(DOCUMENT_III)()(DEC(len), size+EXPAND(FIRST1(EL(t,k,klen,v))), __VA_ARGS__)  NOTFIRST1(EL(t,k,klen,v))
-#define DOCUMENT_III() DOCUMENT_IT
-#define EL(t, k, klen, v) PRIMITIVE_CAT(E, t)(k, klen, v)
-#define DOCUMENT_HEAD(len, size, ...) TO_ENDIANED_ARRAY(((sint32_t){.value=(size)}).string, 4)
+#define BS_FIRST1(...) BS_FIRST(__VA_ARGS__)
+#define BS_FIRST(e, ...) e
+#define BS_NOTFIRST1(...) BS_NOTFIRST(__VA_ARGS__)
+#define BS_NOTFIRST(e, ...) __VA_ARGS__
+#define BSON_DOCUMENT(len, ...) BS_EVAL(BS_OBSTRUCT(BS_DOCUMENT_IT)(len, 5, __VA_ARGS__) '\x0')
+#define BS_DOCUMENT_IT(len, size, ...) BS_IF(len)(BS_DOCUMENT_BODY, BS_DOCUMENT_HEAD)(len, size, __VA_ARGS__)
+#define BS_DOCUMENT_BODY(len, size, t, k, klen, v, ...) \
+  BS_OBSTRUCT(\
+    BS_DOCUMENT_III)()(BS_DEC(len), \
+    size+BS_EXPAND(BS_FIRST1(BS_EL(t,k,klen,v))), \
+    __VA_ARGS__\
+  ) \
+  BS_NOTFIRST1(BS_EL(t,k,klen,v))
+#define BS_DOCUMENT_III() BS_DOCUMENT_IT
+#define BS_EL(t, k, klen, v) BS_PRIMITIVE_CAT(BS_E, t)(k, klen, v)
+#define BS_DOCUMENT_HEAD(len, size, ...) BS_TO_ENDIANED_ARRAY(((sint32_t)(int32_t)(size)).string, 4)
 
-#define EINT32(k, klen, v)  sizeof(k)+5, '\x10', TO_ARRAY(k, klen)  '\x0', TO_ENDIANED_ARRAY(((sint32_t)v).string, 4)
-#define EINT64(k, klen, v)  sizeof(k)+9, '\x12', TO_ARRAY(k, klen) '\x0', TO_ENDIANED_ARRAY(((sint64_t)v).string, 8)
+#define BS_EINT32(k, klen, v)  \
+  sizeof(k)+5, '\x10', \
+  BS_TO_ARRAY(k, klen)  '\x0', \
+  BS_TO_ENDIANED_ARRAY(((sint32_t)v).string, 4)
+#define BS_EINT64(k, klen, v)  \
+  sizeof(k)+9, '\x12', \
+  BS_TO_ARRAY(k, klen) '\x0', \
+  BS_TO_ENDIANED_ARRAY(((sint64_t)v).string, 8)
 
 #include <stdint.h>
-#define stringify(t) union{\
+#ifdef __cplusplus
+#define stringify(t) union BS_PRIMITIVE_CAT(ss,t){\
+  BS_PRIMITIVE_CAT(ss,t)(t in):value(in){};\
   t value;\
   uint8_t string[sizeof(t)];\
 }
+#else
+#define stringify(t) union BS_PRIMITIVE_CAT(ss,t){\
+  t value;\
+  uint8_t string[sizeof(t)];\
+}
+#endif
+
 typedef stringify(int32_t) sint32_t;
 typedef stringify(int64_t) sint64_t;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- test.py
+++ test.py
@@ -22,44 +22,58 @@ class EncodeTest(unittest.TestCase):
             odict([("k4", 0xDEADBEEFFEED)]),
             odict([("k5", -0x0EADBEEFFEED)])
         ]
+        self.compilers = ["gcc", "g++", "clang", "clang++"]
 
     def tearDown(self):
         self._tempdir.cleanup()
 
     def test_encode(self):
-        for d in self.data:
-            self.make_exe(self.target, d)
-            encoded = ''.join(["\\x{:x}".format(x) for x in bson.dumps(d)])
-            encoded_c = check_output([self.target]).decode("utf-8")
-            self.assertEqual(encoded, encoded_c)
+        for cc in self.compilers:
+            for d in self.data:
+                self.make_exe(self.target, lambda: self.generate_string(d), cc=cc)
+                encoded = ''.join(["\\x{:x}".format(x) for x in bson.dumps(d)])
+                encoded_c = check_output([self.target]).decode("utf-8")
+                self.assertEqual(encoded, encoded_c, msg="test encode on {cc} data: {d}".format(cc=cc, d=d))
+
+    def test_encode_two(self):
+        for cc in self.compilers:
+            for d in self.data:
+                g = lambda: self.generate_string(d) + ", " + self.generate_string(d)
+                self.make_exe(self.target, g, cc=cc)
+                encoded = ''.join(["\\x{:x}".format(x) for x in bson.dumps(d)])
+                encoded_c = check_output([self.target]).decode("utf-8")
+                self.assertEqual(encoded+encoded, encoded_c, msg="test two on {cc} data: {d}".format(cc=cc, d=d))
 
     def test_decode(self):
-        for d in self.data:
-            self.make_exe(self.target, d)
-            encoded_c = map(lambda x: x.zfill(2) if x!='' else '' , check_output([self.target]).decode("utf-8").split("\\x"))
-            encoded_c = bytes(bytearray.fromhex(" ".join(encoded_c)))
-            self.assertEqual(d, bson.loads(encoded_c))
+        for cc in self.compilers:
+            for d in self.data:
+                self.make_exe(self.target, lambda: self.generate_string(d), cc=cc)
+                encoded_c = map(lambda x: x.zfill(2) if x!='' else '' , check_output([self.target]).decode("utf-8").split("\\x"))
+                encoded_c = bytes(bytearray.fromhex(" ".join(encoded_c)))
+                self.assertEqual(d, bson.loads(encoded_c), msg="test decode on {cc} data: {d}".format(cc=cc, d=d))
 
-    def make_exe(self, target, d):
+    def make_exe(self, target, d, cc="gcc"):
         targetc = target + '.c'
-        macro = self.generate_string(d)
+        macro = d()
         self.make_file(targetc, macro)
-        check_call(["gcc", targetc, "-I", os.path.dirname(os.path.abspath(__file__)), "-o", target])
+        check_call([cc, targetc, "-I", os.path.dirname(os.path.abspath(__file__)), "-o", target])
 
     def generate_string(self, d):
         r = ""
         for k,v in reversed(d.items()):
             t = "INT32" if bytes_needed(v) <= 4 else "INT64"
             r += "{0}, \"{1}\", {2}, {3}, ".format(t, k, len(k), v)
-        return "DOCUMENT({0}, {1})".format(len(d.keys()), r)
+        return "BSON_DOCUMENT({0}, {1})".format(len(d.keys()), r)
 
     def make_file(self, of, test_line):
-        with open("test.c") as base, open(of, "w") as out:
+        with open("test.ctemplate") as base, open(of, "w") as out: #, open("test.ccc", "w") as ff:
             for line in base:
                 if line.startswith("//EDITHERE"):
                     out.write(test_line)
+#                    ff.write(test_line)
                 else:
                     out.write(line)
+#                    ff.write(line)
 
 if __name__ == "__main__":
     unittest.main()
